24d59d184154c9e5ef9544822ff9f6ab4d6ac93e
TustinLul <149511221+TustinLul@users.noreply.github.com>
2024-04-15 10:05:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+ function greeting() {
+    let name = prompt('Hello, what is your name?');
+    console.log("User's name is " + name);
+    alert('welcome ' + name);
+
+    alert('Thanks for visiting ' + name);
+
+}
+
+
+function Truths1Lie() {
+    if (confirm('I grew up in hawaii')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+
+    if (confirm('I lived in Allen, TX')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+
+    if (confirm('I drive a BMW')){
+        alert('Not quite');
+    } else {
+        alert('Good Job');
+    }
+}
+
+function QuestionThatWantsNumberValue() {
+    let age = prompt('What is my age');
+    while (age != 17) {
+        console.log(age);
+        if (age < 17){
+            alert("Higher, Try Again");
+            let age = prompt('What is my age');
+        } else if (age > 17) {   
+            alert('Lower, Try Again');
+            let age = prompt('What is my age');
+        } else { 
+            alert('Good Job');
+        }
+    }    
+}
+
+function YesOrNoQuestions() {
+    if (confirm('Is the sky blue?')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+
+    if (confirm('Is Cait a goofer')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+
+    if (confirm('Is BMW better than Mercedes')){
+        alert('Not quite');
+    } else {
+        alert('Good Job');
+    }
+
+    if (confirm('Are cars cool?')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+
+    if (confirm('Can Tustin fix cars?')){
+        alert('Good Job');
+    } else {
+        alert('Not quite');
+    }
+      
+}
+
